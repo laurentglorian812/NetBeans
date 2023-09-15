5a9c0a2829f3e86b30ea5c60b87e584638dd33d0
Laurent Glorian <laurentglorian812@gmail.com>
2023-09-15 19:31:27 +0700

    Add files via upload

---
+++ Contoh4/src/contoh4/Contoh4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contoh4;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class Contoh4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmContoh4 n = new FrmContoh4();
+        n.setVisible(true);
+    }
+    
+}
