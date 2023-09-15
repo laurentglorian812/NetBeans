1e15d4fef40a1a3cbb9b6d0f15a40b5670590b16
Laurent Glorian <laurentglorian812@gmail.com>
2023-09-15 20:44:33 +0700

    Add files via upload

---
+++ LatihanCabang5/src/latihancabang5/LatihanCabang5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihancabang5;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class LatihanCabang5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmLatihanCabang5 n = new FrmLatihanCabang5();
+        n.setVisible(true);
+    }
+    
+}
---
+++ LatihanCabang6/src/latihancabang6/LatihanCabang6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihancabang6;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class LatihanCabang6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmLatihanCabang6 n = new FrmLatihanCabang6();
+        n.setVisible(true);
+    }
+    
+}
