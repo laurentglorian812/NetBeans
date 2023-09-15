845694143987067adee72143a21ce16d7890589b
Laurent Glorian <laurentglorian812@gmail.com>
2023-09-15 20:14:11 +0700

    Add files via upload

---
+++ ContohListMulti/src/contohlistmulti/ContohListMulti.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohlistmulti;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class ContohListMulti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmContohListMulti n = new FrmContohListMulti();
+        n.setVisible(true);
+    }
+    
+}
---
+++ ContohTipeData1/src/contohtipedata1/ContohTipeData1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohtipedata1;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class ContohTipeData1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmContohTipeData1 n = new FrmContohTipeData1();
+        n.setVisible(true);
+    }
+    
+}
