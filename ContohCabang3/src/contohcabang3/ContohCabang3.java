8f3fb4bc7192f8bb60be6196d083a492f6f2b9ba
Laurent Glorian <laurentglorian812@gmail.com>
2023-09-15 19:37:14 +0700

    Add files via upload

---
+++ ContohCabang3/src/contohcabang3/ContohCabang3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohcabang3;
+
+/**
+ *
+ * @author Laurent Glorian - 2019130004
+ */
+public class ContohCabang3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmContohCabang3 n = new FrmContohCabang3();
+        n.setVisible(true);
+    }
+    
+}
